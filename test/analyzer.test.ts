3e3f9f75f81853306a2b40e3f70426f65818ac29
Isamu Mogi <isamu@leafytree.jp>
2014-12-09 22:37:10 +0900

    Add test for Analyzer#atk

---
+++ test/analyzer.test.ts
@@ -0,0 +1,20 @@
+///<reference path='../typings/node/node.d.ts'/>
+///<reference path='../typings/jasmine/jasmine.d.ts' />
+
+describe("Analyzer", () => {
+    var octokit
+
+    beforeEach(() => {
+        octokit = new (require("../lib/octokit.ts")).Octokit('http://127.0.0.1:3000')
+    })
+
+    describe("#atk", () => {
+        it("works!", (done) => {
+            var analyzer = new (require("../lib/analyzer.ts")).Analyzer(octokit, "hello", () => {}, () => {})
+            analyzer.atk((atk) => {
+                expect(atk).toBe(43)
+                done()
+            })
+        })
+    })
+})
