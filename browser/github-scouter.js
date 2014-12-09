446c3ac69147935ebdd68fc7a9483b6e6955fbb4
Isamu Mogi <isamu@leafytree.jp>
2014-12-09 22:41:10 +0900

    Update browser package

--- browser/github-scouter.js
+++ browser/github-scouter.js
@@ -208,7 +208,7 @@
 	                return _this.repositoryAtk(repository);
 	            }).reduce(function (l, r) {
 	                return l + r;
-	            }).value();
+	            }).value() || 0;
 	            callback(atk);
 	        });
 	    };
@@ -7466,8 +7466,8 @@
 	 * Module dependencies.
 	 */
 	
-	var Emitter = __webpack_require__(9);
-	var reduce = __webpack_require__(8);
+	var Emitter = __webpack_require__(8);
+	var reduce = __webpack_require__(9);
 	
 	/**
 	 * Root reference for iframes.
@@ -8593,35 +8593,6 @@
 
 	
 	/**
-	 * Reduce `arr` with `fn`.
-	 *
-	 * @param {Array} arr
-	 * @param {Function} fn
-	 * @param {Mixed} initial
-	 *
-	 * TODO: combatible error handling?
-	 */
-	
-	module.exports = function(arr, fn, initial){  
-	  var idx = 0;
-	  var len = arr.length;
-	  var curr = arguments.length == 3
-	    ? initial
-	    : arr[idx++];
-	
-	  while (idx < len) {
-	    curr = fn.call(null, curr, arr[idx], ++idx, arr);
-	  }
-	  
-	  return curr;
-	};
-
-/***/ },
-/* 9 */
-/***/ function(module, exports, __webpack_require__) {
-
-	
-	/**
 	 * Expose `Emitter`.
 	 */
 	
@@ -8785,6 +8756,35 @@
 	  return !! this.listeners(event).length;
 	};
 
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	
+	/**
+	 * Reduce `arr` with `fn`.
+	 *
+	 * @param {Array} arr
+	 * @param {Function} fn
+	 * @param {Mixed} initial
+	 *
+	 * TODO: combatible error handling?
+	 */
+	
+	module.exports = function(arr, fn, initial){  
+	  var idx = 0;
+	  var len = arr.length;
+	  var curr = arguments.length == 3
+	    ? initial
+	    : arr[idx++];
+	
+	  while (idx < len) {
+	    curr = fn.call(null, curr, arr[idx], ++idx, arr);
+	  }
+	  
+	  return curr;
+	};
 
 /***/ }
 /******/ ])
