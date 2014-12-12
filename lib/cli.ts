feb04d498592777949d5d772abb042d5f72e796c
Isamu Mogi <isamu@leafytree.jp>
2014-12-13 00:28:32 +0900

    Add option for JSON output

--- lib/cli.ts
+++ lib/cli.ts
@@ -2,15 +2,16 @@
 ///<reference path='./github-scouter.ts'/>
 
 require('typescript-require')()
-
-var userName = process.argv[2]
-if (!userName) {
-    console.log("Usage: github-scouter [GitHub ID]")
-    process.exit(1)
-}
+var argv = require('yargs')
+    .usage('Usage: $0 [GitHub ID]')
+    .example('$0 saturday06', "Show saturday06's power level")
+    .demand(1)
+    .boolean('json')
+    .describe('json', 'Output JSON')
+    .argv
 
 var scouter = new (require('./github-scouter.ts')).GithubScouter()
-scouter.measure(userName, (powerLevel) => {
+scouter.measure(argv._, (powerLevel) => {
     console.log(powerLevel.toString())
     process.exit(0)
 }, (error) => {
