32cfedaf347e20df6dc91f67219c5d88784c9a00
Isamu Mogi <isamu@leafytree.jp>
2014-12-29 22:32:17 +0900

    Parameters for github api server and the cache server

--- lib/cli.ts
+++ lib/cli.ts
@@ -12,12 +12,18 @@ var argv = require('yargs')
     .describe('json', 'Output JSON')
     .boolean('cache')
     .default('cache', true)
-    .describe('cache', 'Use the cache server (' + m.CACHE_URL + ')')
+    .describe('cache', 'Use the cache server')
     .string('token')
     .describe('token', 'Personal API Access Token')
+    .string('api-url')
+    .default('api-url', m.API_URL)
+    .describe('api-url', 'URL to GitHub API')
+    .string('cache-url')
+    .default('cache-url', m.CACHE_URL)
+    .describe('cache-url', 'URL to the cache server')
     .argv
 
-var scouter = new m.GithubScouter(m.API_URL, argv.cache ? m.CACHE_URL : null, argv.token)
+var scouter = new m.GithubScouter(argv.apiUrl, argv.cache ? argv.cacheUrl : null, argv.token)
 scouter.measure(argv._, (powerLevel) => {
     if (argv.json) {
         console.log(powerLevel.toJSONString())
